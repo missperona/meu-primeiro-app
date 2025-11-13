3a2cf1ce7f1767d89944be850b280eef301e4fa5
missperona <raquel.n.silva2004@gmail.com>
2025-11-13 10:31:02 -0300

    a

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title("Meu primeiro app 🌹")
+
+st.header ('Vamos fazer algo com interatividade')
+
+n = st.number_imput('Entre com um número')
+
+st.write(f'O numero que você escolheu ao quadrado é (n**2)')
